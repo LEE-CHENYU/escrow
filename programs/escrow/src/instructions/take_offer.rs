e707f43baf6f2dc1422e6d01ee352529f391efef
Chenyu Li <60654356+LEE-CHENYU@users.noreply.github.com>
2024-11-05 16:14:14 -0500

    .

--- programs/escrow/src/instructions/take_offer.rs
+++ programs/escrow/src/instructions/take_offer.rs
@@ -34,7 +34,7 @@ pub struct TakeOffer<'info> {
     #[account(
         mut,
         associated_token::mint = token_mint_b,
-        associated_token::authority = maker,
+        associated_token::authority = taker,
         associated_token::token_program = token_program,
     )]
     pub taker_token_account_b: Box<InterfaceAccount<'info, TokenAccount>>,
@@ -72,7 +72,7 @@ pub struct TakeOffer<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn send_wanted_tokens_to_maker(ctx: Context<TakeOffer>) -> Result<()> {
+pub fn send_wanted_tokens_to_maker(ctx: &Context<TakeOffer>) -> Result<()> {
     let transfer_accounts = TransferChecked {
         from: ctx.accounts.taker_token_account_b.to_account_info(),
         mint: ctx.accounts.token_mint_b.to_account_info(),
@@ -81,13 +81,55 @@ pub fn send_wanted_tokens_to_maker(ctx: Context<TakeOffer>) -> Result<()> {
     };
 
     let cpi_ctx = CpiContext::new(
-        ctx.accounts.token_program.to_account_info(), 
+        ctx.accounts.token_program.to_account_info(),
         transfer_accounts,
     );
 
     transfer_checked(
-        cpi_ctx, 
-        ctx.accounts.offer.token_b_wanted_amount, 
+        cpi_ctx,
+        ctx.accounts.offer.token_b_wanted_amount,
         ctx.accounts.token_mint_b.decimals,
     )
+}
+
+pub fn withdraw_and_close_vault(ctx: Context<TakeOffer>) -> Result<()> {
+    let signer_seeds: [&[&[u8]]; 1] = [&[
+        b"offer",
+        ctx.accounts.maker.to_account_info().key.as_ref(),
+        &ctx.accounts.offer.id.to_le_bytes()[..],
+        &[ctx.accounts.offer.bump],
+    ]];
+
+    let accounts = TransferChecked {
+        from: ctx.accounts.vault.to_account_info(),
+        mint: ctx.accounts.token_mint_a.to_account_info(),
+        to: ctx.accounts.taker_token_account_a.to_account_info(),
+        authority: ctx.accounts.offer.to_account_info(),
+    };
+
+    let cpi_context = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        accounts,
+        &signer_seeds,
+    );
+
+    transfer_checked(
+        cpi_context,
+        ctx.accounts.vault.amount,
+        ctx.accounts.token_mint_a.decimals,
+    )?;
+
+    let accounts = CloseAccount {
+        account: ctx.accounts.vault.to_account_info(),
+        destination: ctx.accounts.taker.to_account_info(),
+        authority: ctx.accounts.offer.to_account_info(),
+    };
+
+    let cpi_context = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        accounts,
+        &signer_seeds,
+    );
+
+    close_account(cpi_context)
 }
